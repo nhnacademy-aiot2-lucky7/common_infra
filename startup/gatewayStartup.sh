103969fb8f6a8c2f72dca7ab266305de8ac9c121
st120712 <zzw123@naver.com>
2025-04-08 14:30:44 +0900

    startup setting 변경

--- startup/gatewayStartup.sh
+++ startup/gatewayStartup.sh
@@ -2,4 +2,4 @@ pkill -f 'gateway-0.0.1-SNAPSHOT.jar'
 /usr/local/java/java21/bin/java -Dserver.port=10232 -jar ~/target/gateway-0.0.1-SNAPSHOT.jar > ~/log 2>&1 &
 
 #권한 755 설정
-chmod 755 ~/startup/gatewayStratup.sh
+chmod 755 ~/startup/gatewayStartup.sh
