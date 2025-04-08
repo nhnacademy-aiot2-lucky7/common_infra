fcde0e5df1e753e5be58e07e92a1261314b0cd79
st120712 <zzw123@naver.com>
2025-04-08 13:57:07 +0900

    startup setting 변경

--- startup/eurekaStartup.sh
+++ startup/eurekaStartup.sh
@@ -2,4 +2,4 @@ pkill -f 'eureka-0.0.1-SNAPSHOT.jar'
 /usr/local/java/java21/bin/java -Dserver.port=10233 -jar ~/target/eureka-0.0.1-SNAPSHOT.jar > ~/log 2>&1 &
 
 #권한 755 설정
-chmod 755 ~/eurekaStartup.sh
+chmod 755 ~/startup/eurekaStartup.sh
--- startup/frontStartup.sh
+++ startup/frontStartup.sh
@@ -2,4 +2,4 @@ pkill -f 'lastFrontProject-0.0.1-SNAPSHOT.jar'
 /usr/local/java/java21/bin/java -Dserver.port=10231 -jar ~/target/lastFrontProject-0.0.1-SNAPSHOT.jar > ~/log 2>&1 &
 
 #권한 755 설정
-chmod 755 ~/frontStartup.sh
+chmod 755 ~/startup/frontStartup.sh
